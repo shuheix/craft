73aafe73de71224265aee8801a13c462b0ffc04a
onos <ono.shuhei210@gmail.com>
2021-06-06 22:36:04 +0900

    start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-100.times do |n|
+20.times do |n|
   Company.create!(
     name: "company_name_#{n}",
     email: "abcde@_#{n}"
@@ -7,9 +7,28 @@
     name: "name_#{n}",
     email: "abc@_#{n}"
   )
+end
+
+5.times do |n|
   Article.create!(
     name: "name_is_#{n}番目の名前",
     text: "name_is_#{n}番目の記事",
     user_id: 1
+  )
+end
+
+5.times do |n|
+  Article.create!(
+    name: "name_is_#{n}番目の名前",
+    text: "name_is_#{n}番目の記事",
+    user_id: 2
+  )
+end
+
+5.times do |n|
+  Article.create!(
+    name: "name_is_#{n}番目の名前",
+    text: "name_is_#{n}番目の記事",
+    user_id: 3
   )
 end
