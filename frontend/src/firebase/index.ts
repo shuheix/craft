da64cd5e922440640ae35e85ab6b2e1ce6ed7fb8
onos <ono.shuhei210@gmail.com>
2021-06-20 20:12:47 +0900

    ignore

--- frontend/src/firebase/index.ts
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-
-import "firebase/auth";
-import { firebaseConfig } from "./config";
-
-firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
