4695864901bc550ea41a83f2db9f03751fc29e16
onos <ono.shuhei210@gmail.com>
2021-07-06 08:45:26 +0900

    article詳細画面作成

--- frontend/src/components/layouts/ArticleLayout.tsx
+++ frontend/src/components/layouts/ArticleLayout.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, VFC } from "react";
 import { useParams } from "react-router-dom";
 import { useArticleApi } from "../../hooks/useArticleApi";
 import { articleApi } from "../../constant/railsRoute";
-import { Box, Spinner, Text, Textarea } from "@chakra-ui/react";
+import { Box, Container, Heading, Spinner, Text } from "@chakra-ui/react";
 
 const ArticleLayout: VFC = () => {
   const { articleId } = useParams<{ articleId: string }>();
   const { loading, error, articles, fetchArticleApi } = useArticleApi();
+
+  const articleTitle = articles.map((article) => article.id);
+  const articleText = articles.map((article) => article.text);
 
   useEffect(() => {
     fetchArticleApi(articleApi(articleId));
@@ -14,18 +17,20 @@ const ArticleLayout: VFC = () => {
 
   if (error) return <div>Error!</div>;
   return (
-    <>
+    <Container>
       {loading ? (
         <Box>
           <Spinner />
         </Box>
       ) : (
         <Box>
-          <Text></Text>
-          <Textarea>test</Textarea>
+          <Heading border="solid" borderRadius="full">
+            {articleTitle}
+          </Heading>
+          <Text>{articleText}</Text>
         </Box>
       )}
-    </>
+    </Container>
   );
 };
 
