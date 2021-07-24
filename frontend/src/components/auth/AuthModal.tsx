1a2340bf0a233a62741fd204de8923f8cc6826d5
onos <ono.shuhei210@gmail.com>
2021-07-25 08:49:54 +0900

    modalをタブ分割_#10

--- frontend/src/components/auth/AuthModal.tsx
+++ frontend/src/components/auth/AuthModal.tsx
@@ -3,15 +3,17 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
   useDisclosure,
   Button,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
 import { useAuth } from "../../hooks/useAuth";
-import SignUp from "./modalContent/SignUp";
+import SignUpBody from "./modalContent/SignUpBody";
+import LoginBody from "./modalContent/LoginBody";
 
 const AuthModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -19,7 +21,6 @@ const AuthModal = () => {
     handleSignUpState,
     signUp,
     login,
-    logout,
     username,
     email,
     password,
@@ -28,33 +29,36 @@ const AuthModal = () => {
 
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <Button onClick={onOpen}>Login</Button>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size="md">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader mx="auto">新規登録</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <SignUp
-              username={username}
-              email={email}
-              password={password}
-              passwordConfirmation={passwordConfirmation}
-              handleSignUpState={handleSignUpState}
-            />
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={signUp} mx="auto" w="xs">
-              登録
-            </Button>
-            <Button colorScheme="blue" mr={3} onClick={login} mx="auto" w="xs">
-              ログイン
-            </Button>
-            <Button colorScheme="blue" mr={3} onClick={logout} mx="auto" w="xs">
-              ログアウト
-            </Button>
-          </ModalFooter>
+          <Tabs isFitted variant="enclosed" size="lg">
+            <TabList>
+              <Tab>Login</Tab>
+              <Tab>新規登録</Tab>
+            </TabList>
+            <TabPanels h="400px">
+              <TabPanel>
+                <LoginBody
+                  login={login}
+                  email={email}
+                  password={password}
+                  handleSignUpState={handleSignUpState}
+                />
+              </TabPanel>
+              <TabPanel>
+                <SignUpBody
+                  handleSignUpState={handleSignUpState}
+                  signUp={signUp}
+                  username={username}
+                  email={email}
+                  password={password}
+                  passwordConfirmation={passwordConfirmation}
+                />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </ModalContent>
       </Modal>
     </>
