14a042c4e8211340dac4436014ca0befb0c1ab63
onos <ono.shuhei210@gmail.com>
2021-07-26 06:17:48 +0900

    auth修正

--- frontend/src/components/auth/AuthModal.tsx
+++ frontend/src/components/auth/AuthModal.tsx
@@ -3,22 +3,19 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
   useDisclosure,
   Button,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
 import { useAuth } from "../../hooks/useAuth";
-<<<<<<< Updated upstream
-import SignUp from "./modalContent/SignUp";
-=======
 import SignUpBody from "./modalContent/SignUpBody";
 import LoginBody from "./modalContent/LoginBody";
 import { useGoogleAuth } from "../../hooks/useGoogleAuth";
 import { useGuestAuth } from "../../hooks/useGuestAuth";
->>>>>>> Stashed changes
 
 const AuthModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -42,30 +39,6 @@ const AuthModal = () => {
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent>
-<<<<<<< Updated upstream
-          <ModalHeader mx="auto">新規登録</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <SignUp
-              username={username}
-              email={email}
-              password={password}
-              passwordConfirmation={passwordConfirmation}
-              handleSignUpState={handleSignUpState}
-            />
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={signUp} mx="auto" w="xs">
-              登録
-            </Button>
-            <Button colorScheme="blue" mr={3} onClick={login} mx="auto" w="xs">
-              ログイン
-            </Button>
-            <Button colorScheme="blue" mr={3} onClick={logout} mx="auto" w="xs">
-              ログアウト
-            </Button>
-          </ModalFooter>
-=======
           <Tabs isFitted variant="enclosed" size="lg">
             <TabList>
               <Tab>Login</Tab>
@@ -94,7 +67,6 @@ const AuthModal = () => {
               </TabPanel>
             </TabPanels>
           </Tabs>
->>>>>>> Stashed changes
         </ModalContent>
       </Modal>
     </>
