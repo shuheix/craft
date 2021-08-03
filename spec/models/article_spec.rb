469290bcb2cca817d508fbd7872350dd0cec8bcb
onos <ono.shuhei210@gmail.com>
2021-08-04 07:16:00 +0900

    article_spec_記事作成時のテスト_#15

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe "article登録機能" do
-
     context "titleとtextとuser_idがあればは有効な状態である" do
       it "titleとtextとuser_idがある場合" do
         valid_article = FactoryBot.build(:article)
@@ -11,24 +10,22 @@ RSpec.describe Article, type: :model do
     end
 
     context "titleとtextとuser_idがない場合は無効な状態である" do
-      before do
-        article = FactoryBot.build(:article)
-      end
-
       it "titleがない場合" do
-
+        invalid_article = FactoryBot.build(:article, title: nil)
+        invalid_article.valid?
+        expect(invalid_article.errors[:title]).to include("を入力してください")
       end
 
       it "textがない場合" do
-
+        invalid_article = FactoryBot.build(:article, text: nil)
+        invalid_article.valid?
+        expect(invalid_article.errors[:text]).to include("を入力してください")
       end
       it "user_idがない場合" do
-
+        invalid_article = FactoryBot.build(:article, user_id: nil)
+        invalid_article.valid?
+        expect(invalid_article.errors[:user_id]).to include("を入力してください")
       end
-    end
-
-    context do
-
     end
   end
 end
