420107e0e202282ae4b4512781df75dbb8c3bf84
onos <ono.shuhei210@gmail.com>
2021-08-07 19:20:59 +0900

    articles_requestspec_index成功_#15

--- spec/requests/api/v1/articles_api_spec.rb
+++ spec/requests/api/v1/articles_api_spec.rb
@@ -2,21 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::ArticlesControllers", type: :request do
   describe "#index" do
-    context "ユーザーがログインしていない場合"
-      it "ステータスコード「200 OK」を返す"
-      it "成功時はJSONデータのレスポンスを返す"
-      it "1つ以上のarticleデータを返す"
+    # API仕様
+    # ユーザーのログイン状態の有無に関わらずindexメソッドは、値を返す
+
+    context "ユーザーがログインしていない場合" do
+      it "ステータスコード「200 OK」を返す" do
+        get '/api/v1/articles'
+        expect(response).to have_http_status(:ok)
+      end
+      it "成功時はarticlesのキーを持つJSONデータのレスポンスを返す" do
+        get '/api/v1/articles'
+        expect(JSON.parse(response.body)).to include("articles" => [])
+      end
     end
 
-    context "ユーザーがログインしていない場合"
-      it "ステータスコード「200 OK」を返す"
-      it "成功時はJSONデータのレスポンスを返す"
-      it "1つ以上のarticleデータを返す"
+    context "ユーザーがログインしている場合" do
+      it "ステータスコード「200 OK」を返す" do
+        get '/api/v1/articles',params{}
+        expect(response).to have_http_status(:ok)
+      end
+      it "成功時はarticlesのキーを持つJSONデータのレスポンスを返す" do
+        get '/api/v1/articles'
+        expect(JSON.parse(response.body)).to include("articles" => [])
+      end
     end
 
-    context "リクエストパラメータが異常の場合"
-      it "ステータスコードは「400 Badrequest」を返す"
-      it "パラメータが不正のJSONレスポンスを返す"
-      it "articleのデータを返さない"
+    context "リクエストパラメータが異常の場合" do
+      it "ステータスコードは「400 Badrequest」を返す" do
+      end
+      it "パラメータが不正のJSONレスポンスを返す" do
+      end
+      it "articleのデータを返さない" do
+      end
     end
+  end
 end
