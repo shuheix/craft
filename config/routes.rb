51cf59c746f761e7767cfc0c5757eca0e709b71e
onos <ono.shuhei210@gmail.com>
2021-08-09 21:45:09 +0900

    user_show_api作成_#24

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create,:destroy,:update]
+      resources :users, only: [:create,:destroy,:update,:show]
       resources :articles do
         resource :favorites, only: [:create, :destroy]
         resource :comments
