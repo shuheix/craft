93ef6e5b57d9f786db27f2aecd2a516e2bb3982d
onos <ono.shuhei210@gmail.com>
2021-08-20 07:41:30 +0900

    次へボタン追加

--- frontend/src/components/aside/LeftAside.tsx
+++ frontend/src/components/aside/LeftAside.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 import React, { VFC } from "react";
 
 const Aside: VFC = () => {
-  return <Box bgColor="green.100" w={280} minH="50px"></Box>;
+  return <Box bgColor="green.100" w={600} minH="50px"></Box>;
 };
 
 export default Aside;
