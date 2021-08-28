b9dc30760cc54b7eb15064f31f28861495227c15
onos <ono.shuhei210@gmail.com>
2021-08-29 06:56:36 +0900

    formの分割_#33

--- frontend/src/components/pages/ShowArticlePage.tsx
+++ frontend/src/components/pages/ShowArticlePage.tsx
@@ -1,22 +1,12 @@
-import {
-  Box,
-  Button,
-  Container,
-  Flex,
-  Spinner,
-  Textarea,
-  useDisclosure,
-} from "@chakra-ui/react";
-import axios from "axios";
+import { Box, Container, Flex, Spinner, useDisclosure } from "@chakra-ui/react";
 import React, { useEffect, VFC } from "react";
-import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import { SINGLE_ARTICLE_API } from "../../constant/railsRoute";
-import { auth } from "../../firebase";
 import { useArticle } from "../../hooks/useArticle";
 import { useFetchSingleArticle } from "../../hooks/useFetchSingleArticle";
 import ShowArticleBody from "../article/body/ShowArticleBody";
 import ButtonKit from "../article/ButtonKit";
+import CommentForm from "../article/comment/CommentForm";
 import CommentList from "../article/comment/CommentList";
 import DeleteArticleDialog from "../article/dialog/DeleteArticleDialog";
 import Header from "../header/Header";
@@ -26,27 +16,10 @@ const ArticlePage: VFC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = React.useRef(null);
   const { data, error, loading, fetchSingleArticle } = useFetchSingleArticle();
-  const { register, handleSubmit } = useForm<Comment>();
   const { onClickDestroyButton, onClickEditButton } = useArticle(
     articleId,
     data
   );
-
-  type Comment = {
-    text: string;
-  };
-  const onSubmit = (data: Comment) => {
-    auth.currentUser?.getIdToken(true).then((token) => {
-      axios
-        .post(`http://localhost:3000/api/v1/articles/${articleId}/comments`, {
-          headers: { Authorization: token },
-          text: data.text,
-        })
-        .then(() => {
-          fetchSingleArticle(SINGLE_ARTICLE_API(articleId));
-        });
-    });
-  };
 
   useEffect(() => {
     fetchSingleArticle(SINGLE_ARTICLE_API(articleId));
@@ -69,27 +42,7 @@ const ArticlePage: VFC = () => {
                 <Box w="100%">
                   <ShowArticleBody data={data} />
                   <CommentList data={data} />
-                  <Box mt={10}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                      <Textarea
-                        resize="none"
-                        bgColor="white"
-                        row={20}
-                        boxShadow="sm"
-                        id="text"
-                        borderRadius="2xl"
-                        pr={10}
-                        {...register("text", {
-                          required: "内容が記載されていません",
-                          maxLength: {
-                            value: 1000,
-                            message: "コメントは最大1000文字までです",
-                          },
-                        })}
-                      />
-                      <Button type="submit">投稿</Button>
-                    </form>
-                  </Box>
+                  <CommentForm articleId={articleId} />
                 </Box>
                 <ButtonKit
                   onOpen={onOpen}
