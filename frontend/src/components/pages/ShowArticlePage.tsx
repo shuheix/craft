9fe1aebc15fc9b32cf195537017c29079e9e4605
onos <ono.shuhei210@gmail.com>
2021-09-02 08:31:19 +0900

    コメントリスト取得_#35

--- frontend/src/components/pages/ShowArticlePage.tsx
+++ frontend/src/components/pages/ShowArticlePage.tsx
@@ -8,9 +8,12 @@ import {
   useBoolean,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { useEffect, VFC } from "react";
+import React, { useEffect, useState, VFC } from "react";
 import { useParams } from "react-router-dom";
-import { SINGLE_ARTICLE_API } from "../../constant/railsRoute";
+import {
+  INDEX_COMMENTS_API,
+  SINGLE_ARTICLE_API,
+} from "../../constant/railsRoute";
 import { useArticle } from "../../hooks/useArticle";
 import { useFetchSingleArticle } from "../../hooks/useFetchSingleArticle";
 import ShowArticleBody from "../article/body/ShowArticleBody";
@@ -23,6 +26,9 @@ import Header from "../header/Header";
 import ArticleUser from "../article/aside/ArticleUser";
 import { useFavorite } from "../../hooks/useFavorite";
 import { StarIcon } from "@chakra-ui/icons";
+import { CommentType } from "../../types/commentType";
+import axios from "axios";
+import { auth } from "../../firebase";
 
 const ArticlePage: VFC = () => {
   const { articleId } = useParams<{ articleId: string }>();
@@ -35,12 +41,38 @@ const ArticlePage: VFC = () => {
     articleId,
     data
   );
+  const [comments, setComments] = useState<CommentType[] | null>([]);
 
   const [favorite, setFavorite] = useBoolean();
 
   useEffect(() => {
     fetchSingleArticle(SINGLE_ARTICLE_API(articleId));
   }, [articleId, fetchSingleArticle]);
+
+  useEffect(() => {
+    axios
+      .get<CommentType[] | null>(INDEX_COMMENTS_API(articleId))
+      .then((res) => {
+        setComments(res.data);
+      });
+  }, [articleId]);
+
+  const postComments = () => {
+    auth.currentUser?.getIdToken(true).then((token) => {
+      axios
+        .post(INDEX_COMMENTS_API(articleId), {
+          headers: { Authorization: token },
+          text: comments,
+        })
+        .then(() => {
+          axios
+            .get<CommentType[]>(INDEX_COMMENTS_API(articleId))
+            .then((res) => {
+              setComments(res.data);
+            });
+        });
+    });
+  };
 
   if (error) return <p>error!</p>;
 
@@ -62,6 +94,10 @@ const ArticlePage: VFC = () => {
                   <ShowArticleBody data={data} />
                   <CommentList data={data} />
                   <CommentForm articleId={articleId} />
+                  <p>test</p>
+                  {comments?.map((item) => (
+                    <p>{item.text}</p>
+                  ))}
                 </Box>
                 <Box maxW="300px">
                   <ArticleUser data={data} />
--- frontend/src/constant/railsRoute.ts
+++ frontend/src/constant/railsRoute.ts
@@ -7,6 +7,10 @@ export const INDEX_ARTICLES_API = (page: string | null): string =>
 export const SINGLE_ARTICLE_API = (articleId: string): string =>
   `${defaultApi}/articles/${articleId}`;
 
+// comments_controller
+export const INDEX_COMMENTS_API = (articleId: string): string =>
+  `${defaultApi}/articles/${articleId}/comments`;
+
 // rails_users_controller
 export const USERS_API = `${defaultApi}/users`;
 export const SHOW_USERS_API = (usersId: string) => `${USERS_API}/${usersId}`;
