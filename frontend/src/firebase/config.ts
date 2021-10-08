b9bb257136c62d1d7b9269ddff44fb75bcb2d80b
onos <ono.shuhei210@gmail.com>
2021-10-09 04:35:42 +0900

    env追加

---
+++ frontend/src/firebase/config.ts
@@ -0,0 +1,19 @@
+type Config = {
+  apiKey: string | undefined;
+  authDomain: string | undefined;
+  projectId: string | undefined;
+  storageBucket: string | undefined;
+  messagingSenderId: string | undefined;
+  appId: string | undefined;
+  measurementId: string | undefined;
+};
+
+export const firebaseConfig: Config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
