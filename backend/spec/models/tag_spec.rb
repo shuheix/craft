b84bb2a377a27b6175be93bd62ba056353bfe7d1
onos <ono.shuhei210@gmail.com>
2021-12-01 23:29:31 +0900

    tagモデルのテスト内容を列挙

--- backend/spec/models/tag_spec.rb
+++ backend/spec/models/tag_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Tag作成機能' do
+    context 'nameが1文字以上,10文字以下で且つ、nameが一意であれば有効である' do
+      it 'nameが1文字で、且つ一意の場合' do
+      end
+
+      it '文字数が10文字で、且つ一意の場合' do
+      end
+    end
+
+    context 'nameが0文字、11文字以上、一意でない、いずれかの場合無効である' do
+      it  'nameが0文字の場合' do
+      end
+
+      it 'nameが11文字である場合' do
+      end
+
+      it '一意でない場合' do
+      end
+    end
+  end
 end
