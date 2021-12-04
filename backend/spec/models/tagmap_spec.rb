8b9719193661404cae996f4ca59cf1d161246480
onos <ono.shuhei210@gmail.com>
2021-12-04 23:29:28 +0900

    tagmapモデルのテスト内容を列挙

--- backend/spec/models/tagmap_spec.rb
+++ backend/spec/models/tagmap_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Tagmap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'タグ登録機能' do
+    context 'tag_idとarticle_idの組み合わせが一意であれば有効である' do
+      it 'tag_idとarticle_idの組み合わせが一意である場合' do
+
+      end
+    end
+
+    constet 'tag_idとarticle_idの組み合わせが一意でない場合、無効である' do
+      it 'tag_idとarticle_idの組み合わせが一意でない場合' do
+
+      end
+    end
+  end
 end
