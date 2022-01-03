e03a4d24efab967210bb631a6a21c62809a5b9bb
onos <ono.shuhei210@gmail.com>
2022-01-04 05:14:58 +0900

    production.rbの修正

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "backend"
 set :repo_url, "git@github.com:shuheix/craft.git"
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :branch, ENV['BRANCH'] || "main"
-
+set :bundle_gemfile, "backend/Gemfile"
 # nginxの設定
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
