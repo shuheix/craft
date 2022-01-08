2608590fcc5667d260c53d83553d0e3c6438992f
onos <ono.shuhei210@gmail.com>
2022-01-08 15:40:44 +0900

    capistrano自動デプロイ対応

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -1,17 +1,39 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "backend"
+set :application, "craft"
 set :repo_url, "git@github.com:shuheix/craft.git"
-set :rbenv_ruby, File.read('.ruby-version').strip
-set :branch, ENV['BRANCH'] || "main"
-set :bundle_gemfile, "backend/Gemfile"
-# nginxの設定
-set :nginx_config_name, "#{fetch(:application)}.conf"
-set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
 
 append :linked_files, "config/master.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+# SSH接続設定
+set :ssh_options, {
+  auth_methods: ['publickey'],
+  keys: ['~/.ssh/プライベートキーの名前.pem']
+}
+# 保存しておく世代の設定
+set :keep_releases, 5
+
+# rbenvの設定
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_type, :user
+set :bundle_gemfile, "backend/Gemfile"
+
+# Unicornのプロセスの指定
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの指定
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+# Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- backend/config/deploy/production.rb
+++ backend/config/deploy/production.rb
@@ -1,7 +1,1 @@
 server "54.150.206.249", user: "ec2-user", roles: %w{app db web}
-
-set :ssh_options, {
-  keys: %w(~/.ssh/rails-key.pem),
-  forward_agent: true,
-  auth_methods: %w(publickey),
-}
