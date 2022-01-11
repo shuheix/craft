ccf00bdf752388ce40143f217edf834582ccfd0b
onos <ono.shuhei210@gmail.com>
2022-01-11 21:38:40 +0900

    corsの設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins "https://crafts-app.com", "http://localhost:3001"
 
     resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+            headers: :any,
+            methods: %i[get post put patch delete options head]
   end
 end
