591eb005d5d10dbfdfc4f99dcffb8417bc4fdde3
onos <ono.shuhei210@gmail.com>
2022-01-16 20:29:14 +0900

    ,追加

--- backend/config/initializers/firebase_auth_initializer.rb
+++ backend/config/initializers/firebase_auth_initializer.rb
@@ -1,6 +1,6 @@
 FirebaseIdToken.configure do |config|
   config.redis = Redis.new(
-    host: Rails.application.credentials.redis[:host] || "127.0.0.1"
+    host: Rails.application.credentials.redis[:host] || "127.0.0.1",
     port: "6379"
   )
   config.project_ids = ['craftauth-19733']
