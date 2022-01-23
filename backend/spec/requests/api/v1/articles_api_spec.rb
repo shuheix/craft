f65c64865f74225cd5f5b1870117ce6814cbeb35
onos <ono.shuhei210@gmail.com>
2022-01-23 22:52:39 +0900

    rspec_article#updateのテストを追加

--- backend/spec/requests/api/v1/articles_api_spec.rb
+++ backend/spec/requests/api/v1/articles_api_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::ArticlesControllers', type: :request do
+  # before_actionのAuthenticate_userのスタブする
+  let(:headers){
+    {
+      'Content-Type': 'application/json',
+      Authorization: "Bearer token"
+    }
+  }
+
+  let(:user){ FactoryBot.create(:user) }
+  before { stub_firebase(user) }
+
   describe '#index' do
     context '正常なリクエストの場合、ステータスコード、jsonデータを返す' do
       before do
@@ -19,29 +30,19 @@ RSpec.describe 'Api::V1::ArticlesControllers', type: :request do
 
   describe '#show' do
     context '正常なリクエストの場合、ステータスコード、jsonデータを返す' do
-      let(:article){ FactoryBot.create(:article) }
+      let(:valid_article){ FactoryBot.create(:article) }
       it 'ステータスコード「200 OK」を返す' do
-        get "/api/v1/articles/#{article[:id]}"
+        get "/api/v1/articles/#{valid_article[:id]}"
         expect(response).to have_http_status(:ok)
       end
       it '成功時のJSONデータのレスポンスを返す' do
-        get "/api/v1/articles/#{article[:id]}"
+        get "/api/v1/articles/#{valid_article[:id]}"
         expect(response.content_type).to eq 'application/json; charset=utf-8'
       end
     end
   end
 
   describe '#create' do
-    let(:headers){
-      {
-        'Content-Type': 'application/json',
-        Authorization: "Bearer token"
-      }
-    }
-
-    let(:user){ FactoryBot.create(:user) }
-    before { stub_firebase(user) }
-
     context '全てのパラメータが揃っているリクエストの場合' do
       it 'ステータスコード「200 OK」を返す' do
         post '/api/v1/articles', headers: headers, params: {title: 'a', text: 'a'}.to_json
@@ -88,23 +89,39 @@ RSpec.describe 'Api::V1::ArticlesControllers', type: :request do
   end
 
   describe '#update' do
+    let(:valid_article){ FactoryBot.create(:article) }
     context '全てのパラメータが揃っているリクエストの場合' do
       it 'ステータスコード「200 OK」を返す' do
+        put "/api/v1/articles/#{valid_article.id}", params: { title: "new_title", text: "new_text" }
+        expect(response).to have_http_status(:ok)
       end
-      it '成功時のJSONデータのレスポンスを返す' do
-      end
-      it '記事を更新する' do
+      it '記事が更新される' do
+        put "/api/v1/articles/#{valid_article.id}", params: { title: "new_title", text: "new_text" }
+        json = JSON.parse(response.body)
+        expect(json["articles"]["title"]).to eq("new_title")
       end
     end
     context 'titleパラメータが不足している場合' do
-      it 'ステータスコード「400 Bad Request」を返す'
-      it 'パラメータ不正のJSONデータのレスポンスを返す'
-      it '記事を更新しない'
+      it 'ステータスコード「400 Bad Request」を返す' do
+        put "/api/v1/articles/#{valid_article.id}", params: { title: "", text: "new_text" }
+        expect(response).to have_http_status(:bad_request)
+      end
+      it '記事が更新されない' do
+        put "/api/v1/articles/#{valid_article.id}", params: { title: "", text: "new_text" }
+        article = Article.find(valid_article.id)
+        expect(article.title).to eq('testのタイトル')
+      end
     end
     context 'textパラメータが不足している場合' do
-      it 'ステータスコード「400 Bad Request」を返す'
-      it 'パラメータ不正のJSONデータのレスポンスを返す'
-      it '記事を更新しない'
+      it 'ステータスコード「400 Bad Request」を返す' do
+        put "/api/v1/articles/#{valid_article.id}", params: { title: "new_title", text: "" }
+        expect(response).to have_http_status(:bad_request)
+      end
+      it '記事が更新されない' do
+        put "/api/v1/articles/#{valid_article.id}", params: { title: "new_title", text: "" }
+        article = Article.find(valid_article.id)
+        expect(article.text).to eq('textの内容')
+      end
     end
   end
 
