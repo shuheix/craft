8219194626e2414c227e4f1c0c905706e3285660
onos <ono.shuhei210@gmail.com>
2022-01-31 04:15:46 +0900

    paginationのtype修正

--- frontend/src/types/paginationType.ts
+++ frontend/src/types/paginationType.ts
@@ -2,6 +2,6 @@ export type PaginationType = {
   current_page: number;
   next_page: null | number;
   prev_page: null | number;
-  total_pages: number;
-  total_count: number;
+  total_pages: null | number;
+  total_count: null | number;
 };
