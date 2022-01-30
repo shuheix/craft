1a5ebde0d8db5427600bfc520021c1bfd7ade492
onos <ono.shuhei210@gmail.com>
2022-01-31 04:31:46 +0900

    total_count

--- frontend/src/components/footer/PageSelect.tsx
+++ frontend/src/components/footer/PageSelect.tsx
@@ -15,8 +15,7 @@ type Props = {
 const PageSelect: VFC<Props> = (props) => {
   const { data } = props;
   const { nextPage, prevPage } = usePageSelect();
-  if (data?.meta.total_count === undefined) return null;
-  if (data?.meta.total_count === 0) return null;
+
   return (
     <>
       <Flex justifyContent="flex-end" mt={4} pb={10}>
