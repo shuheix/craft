42304cb8d242df849fd9873f361762900b6ae9d6
onos <ono.shuhei210@gmail.com>
2022-02-09 08:19:01 +0900

    firebase_loginの準備

--- frontend/e2e/cypress/plugins/index.js
+++ frontend/e2e/cypress/plugins/index.js
@@ -11,6 +11,7 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+require("dotev").config();
 
 /**
  * @type {Cypress.PluginConfig}
@@ -19,4 +20,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  config.env = process.env;
+  return config;
+};
