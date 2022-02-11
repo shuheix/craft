9613de09ef968db49eb32270dd5602b650102098
onos <ono.shuhei210@gmail.com>
2022-02-11 17:17:07 +0900

    cypressにfirebaseの環境変数を追加

--- frontend/e2e/cypress/support/commands.js
+++ frontend/e2e/cypress/support/commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -7,7 +8,25 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { attachCustomCommands } from "cypress-firebase";
+
+const fbConfig = {
+  apiKey: Cypress.env("CYPRESS_FB_API_KEY"),
+  authDomain: Cypress.env("CYPRESS_FB_AUTH_DOMAIN"),
+  projectId: Cypress.env("CYPRESS_FB_PROJECT_ID"),
+  storageBucket: Cypress.env("CYPRESS_FB_STORAGE__BUCKET"),
+  messagingSenderId: Cypress.env("CYPRESS_FB_MESSAGING_SENDER_ID"),
+  appId: Cypress.env("CYPRESS_FB_APP_ID"),
+  measurementId: Cypress.env("CYPRESS_FB_MEASUREMENT_ID"),
+};
+
+firebase.initializeApp(fbConfig);
+
+// eslint-disable-next-line no-undef
+attachCustomCommands({ Cypress, cy, firebase });
+
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
