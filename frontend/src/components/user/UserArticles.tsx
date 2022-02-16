f76454db04c0cc7995fb717f7ab2a595c49597fa
onos <ono.shuhei210@gmail.com>
2022-02-16 20:25:12 +0900

    userページの投稿一覧を修正

--- frontend/src/components/user/UserArticles.tsx
+++ frontend/src/components/user/UserArticles.tsx
@@ -8,19 +8,25 @@ import {
   Center,
   HStack,
   VStack,
+  Icon,
 } from "@chakra-ui/react";
-import { SHOW_ARTICLE_URL } from "../../constant/appHistory";
 import { useUser } from "../../hooks/fetch/useUser";
-import { useHistory, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import EditArticleButton from "../ui/button/EditArticleButton";
 import DeleteArticleButton from "../ui/button/DeleteArticleButton";
 import { AuthContext } from "../../providers/AuthProvider";
+import { CalendarIcon } from "@chakra-ui/icons";
+import dayjs from "dayjs";
+import AnswerdStateTag from "../article/tag/AnswerdStateTag";
+import UserAvatar from "./UserAvatar";
+import { useAppHistory } from "../../hooks/useAppHistory";
 
 const UserArticles = () => {
-  const history = useHistory();
   const { uid } = useParams<{ uid: string }>();
   const { data, isError, isLoading } = useUser(uid);
   const { currentUser } = useContext(AuthContext);
+  const { goShowArticlePage } = useAppHistory();
+
   if (isError) return <p>読み込みに失敗しました。</p>;
   if (isLoading) return <Spinner />;
   if (data?.user.articles?.length === 0)
@@ -29,26 +35,43 @@ const UserArticles = () => {
         <p>まだ、投稿はありません。</p>
       </Center>
     );
-
   return (
     <Stack spacing={5}>
       {data?.user?.articles.map((article) => (
         <HStack key={article.id}>
           <Box
-            flexGrow={1}
-            bgColor="white"
-            shadow="md"
-            px={5}
-            py={8}
-            onClick={() => history.push(SHOW_ARTICLE_URL(article.id))}
+            w="100%"
+            key={article.id}
+            onClick={() => goShowArticlePage(article.id)}
+            boxShadow="md"
             _hover={{
-              boxShadow: "lg",
               cursor: "pointer",
+              boxShadow: "xl",
             }}
-            borderRadius="lg"
-            data-cy="article"
+            height="100px"
+            bgColor="white"
+            borderRadius="xl"
           >
-            <Heading fontSize="xl">{article.title}</Heading>
+            <HStack h="100%" px={4}>
+              <UserAvatar src={article.avatar} />
+              <VStack spacing={2} flexGrow={1}>
+                <HStack justifyContent="space-between" w="100%">
+                  <Heading size="xs" alignSelf="flex-start" mr={4}>
+                    <Icon as={CalendarIcon} mr={1} />
+                    {dayjs(article.created_at).format("YYYY年MM月DD日")}
+                  </Heading>
+                  <AnswerdStateTag is_answerd={article.is_answerd} />
+                </HStack>
+                <Heading
+                  size="sm"
+                  maxWidth="100%"
+                  alignSelf="flex-start"
+                  ml={4}
+                >
+                  {article.title}
+                </Heading>
+              </VStack>
+            </HStack>
           </Box>
           {currentUser?.uid === article.uid && (
             <VStack>
