39880e264afb8739067b2880c999530d9205794b
onos <ono.shuhei210@gmail.com>
2022-02-21 02:14:27 +0900

    storybook_install

---
+++ frontend/src/components/header/Header.stories.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+export default {
+  title: "Header",
+  component: "Header",
+};
+
+const Template = () => <Header />;
+
+export const DefaultHeader = Template.bind({});
